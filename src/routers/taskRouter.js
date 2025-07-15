bbc48879a9bd4b3a88b13411382e94b6dc3949d7
arq-angel <87254753+arq-angel@users.noreply.github.com>
2025-07-15 17:21:20 +1000

    code cleanup for part 2

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -12,46 +12,17 @@ const router = express.Router();
 //   next();
 // });
 
-let fakeDB = [
-  {
-    id: 1,
-    task: "Coding",
-    hr: 40,
-    type: "entry",
-  },
-  {
-    id: 2,
-    task: "Gaming",
-    hr: 40,
-    type: "entry",
-  },
-  {
-    id: 3,
-    task: "Cooking",
-    hr: 40,
-    type: "entry",
-  },
-  {
-    id: 4,
-    task: "Washing",
-    hr: 40,
-    type: "entry",
-  },
-];
-
 router.get("/", (req, res, next) => {
   // do your code
+
   res.json({
     status: "success",
     message: "Here is the tasks list ",
-    tasks: fakeDB,
   });
 });
 
 router.post("/", (req, res, next) => {
   // do your code
-  fakeDB.push(req.body);
-  console.log(fakeDB);
 
   res.json({
     status: "success",
@@ -63,15 +34,6 @@ router.patch("/", (req, res, next) => {
   // do your code
   const { id, type } = req.body;
 
-  fakeDB = fakeDB.map((item) => {
-    if (item.id === id) {
-      item.type = type;
-      return item;
-    } else {
-      return item;
-    }
-  });
-
   res.json({
     status: "success",
     message: "Your task has been updated",
@@ -80,10 +42,7 @@ router.patch("/", (req, res, next) => {
 
 router.delete("/:id", (req, res, next) => {
   // do your code
-
   const { id } = req.params;
-
-  fakeDB = fakeDB.filter((item) => item.id !== +id);
 
   res.json({
     status: "success",
