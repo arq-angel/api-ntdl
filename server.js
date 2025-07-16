a5b29aa5b54541bed14d1723cc9278a6074e5059
arq-angel <87254753+arq-angel@users.noreply.github.com>
2025-07-16 14:09:15 +1000

    complete the project finalization and ready to deploy

--- server.js
+++ server.js
@@ -13,7 +13,20 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
+// static serving
+import path from "path";
+const __dirname = path.resolve();
+
+// serve the static files from the node
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+console.log(__dirname);
+
 import taskRouter from "./src/routers/taskRouter.js";
+import { log } from "console";
 app.use("/api/v1/tasks", taskRouter);
 
 app.listen(PORT, (error) => {
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-const mongoUrl = "mongodb://localhost:27017/online_ntdl";
+// const mongoUrl = "mongodb://localhost:27017/online_ntdl";
+const mongoUrl = process.env.MONGO_URL;
 
 export const connectMongoDB = async () => {
   try {
